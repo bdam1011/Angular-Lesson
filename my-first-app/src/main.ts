e23339e1d48841fa8100d3205c843d00a93ed227
harrywang <harrywang@tist.com.tw>
2024-01-30 09:11:16 +0800

    initialize

---
+++ my-first-app/src/main.ts
@@ -0,0 +1,8 @@
+//FIRST CODE TO BE EXCUTED
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
